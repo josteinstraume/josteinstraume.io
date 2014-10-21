35498933f11d81cef8a6f3746d5a411640992124
Jostein Straume <joebarrystraume@gmail.com>
2014-10-21 11:52:32 -0700

    Edit dependencies to deploy to EC2

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,37 +1,33 @@
 'use strict';
 
-/**
- * @ngdoc overview
- * @name josteinstraumeioApp
- * @description
- * # josteinstraumeioApp
- *
- * Main module of the application.
- */
-angular
-  .module('josteinstraumeioApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .when('/myroute', {
-        templateUrl: 'views/myroute.html',
-        controller: 'MyrouteCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+(function() {
+
+  skel.init({
+    reset: 'full',
+    breakpoints: {
+      'global': { range: '*', href: 'styles/style.css', lockViewport: true },
+      'wide': { range: '-1680', href: 'styles/style-wide.css' },
+      'normal': { range: '-1280', href: 'styles/style-normal.css' },
+      'mobile': { range: '-640', href: 'styles/style-mobile.css' },
+      'mobilep': { range: '-360', href: 'styles/style-mobilep.css' }
+    }
   });
+
+  // Events (JS).
+
+    // Remove "loading" class once the page has fully loaded.
+      window.onload = function() {
+        document.body.className = '';
+      }
+
+    // Prevent scrolling on touch.
+      window.ontouchmove = function() {
+        return false;
+      }
+
+    // Fix scroll position on orientation change.
+      window.onorientationchange = function() {
+        document.body.scrollTop = 0;
+      }
+
+})();
